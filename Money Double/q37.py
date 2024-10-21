0bd5a9ac64cd3fb487cabd3e01eaa04ee1ecac48
Karishma-Varshney <143352314+Karishma-Varshney@users.noreply.github.com>
2024-10-21 23:03:16 +0530

    q37 done

---
+++ Money Double/q37.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range(int(input())):
+    x,y=map(int,input().split())
+    
+    for i in range(y):
+        if x<=1000:
+            x+=1000
+        else:
+            x=2*x
+            
+    print(x)
